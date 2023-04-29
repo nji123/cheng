6d1fddb5f39036d4526db7ce03403ab1dc26bf94
zzx <2954124863@qq.com>
2023-04-29 19:03:55 +0800

    Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Big;
+
+public class Main {
+    public static void main(String[] args) {
+        MSA msa=new MSA();
+        int[] array={1,-2,3,5,-3,6,1,-1};
+        msa.Calc(array,8);
+
+    }
+
+}
